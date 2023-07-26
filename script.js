de0e1e6bb82f00a1c2d83dbbc4fbb8150d30a818
Lisandro Alvo <lisandroalvo2@gmail.com>
2023-07-26 16:43:17 -0300

    last changed I made in Brazil 26 Julygit branch

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function changeText() {
 }
 
 setInterval(changeText, 2000); // Change every 2000 milliseconds, or 2 seconds
+
+/* Project cards */
+
+// Animate the project title on page load
+const hamburgerMenu = document.querySelector('#hamburgerMenu');
+const navLinks = document.querySelector('#navLinks');
+
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.style.left = navLinks.style.left === '0%' ? '-100%' : '0%';
+});
+
+/* menu animation logo */
+function toggleMenu() {
+    var menu = document.getElementById("hamburgerMenu");
+    menu.classList.toggle("change");
+}
